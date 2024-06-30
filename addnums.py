6ede28ed4369e259c291b0613559b38914aa5917
lovestherr <estabraqalta@gmail.com>
2024-06-30 17:34:29 +0300

    Added new function

--- addnums.py
+++ addnums.py
@@ -1,6 +1,11 @@
 def add_numbers(a, b):
     return a + b
 
+def sub_numbers(a, b):
+    return a - b
+
 # Example usage
-result = add_numbers(3, 5)
-print(f"The sum of 3 and 5 is: {result}")
+add = add_numbers(3, 5)
+sub = sub_numbers(5, 3)
+print(f"The sum of 3 and 5 is: {add}")
+print(f"The sum of 3 and 5 is: {sub}")
